31949d6e4b892cef04348ca279bd0d16f9d8e1a5
Matthew Olsen <matto@Matthews-MacBook-Pro-2.local>
2019-02-27 10:29:19 -0800

    updates to figure out variables in blade

---
+++ config/constants.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Defined Variables
+    |--------------------------------------------------------------------------
+    |
+    | This is a set of variables that are made specific to this application
+    | that are better placed here rather than in .env file.
+    | Use config('your_key') to get the values.
+    |
+    */
+
+    'company_name' => env('COMPANY_NAME','Acme Inc'),
+    'company_email' => env('COMPANY_email','contact@acme.inc'),
+
+
+];
